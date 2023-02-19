f5bde12e90bf6344494eb6ad1ba3463fc2f60945
iqbalkurnia <iqbalkurnia091@gmail.com>
2023-02-19 23:26:11 +0700

    update request

---
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use App\Models\User;
+// use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use symfony\Component\HttpFoundation\Response;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // create middleware from karnel at here
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required', 'string', 'max:255',
+            ],
+            'email' => [
+                'required', 'email', 'unique:users', 'max:255',
+            ],
+            'name' => [
+                'min:8', 'string', 'max:255', 'mixedCase'
+            ],
+        ];
+    }
+}
